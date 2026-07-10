4b017d58a45a3b5f79dadfa9aed540e399f2caf9
Lucas Smith <smitlucas2563@gmail.com>
2026-07-10 13:42:50 -0400

    Added multi page observation fetching functionality, requirements.txt, README.txt

--- python/update_observation.py
+++ python/update_observation.py
@@ -1,24 +1,31 @@
-# 1. Get species name
-
-# 2. Ask iNat for observations
-
-# 3. Read response
-
-# 4. Print total observations found
-
 import requests
 import json
 
 species_name = 'Callicarpa americana'
 
-def fetch_observations(species_name):
-    url = 'https://api.inaturalist.org/v1/observations'
+def fetch_all_observations(species_name, max_pages=10):
+    all_observations = []
 
+    for page in range(1, max_pages + 1):
+        observations = fetch_observations(species_name, page=page)
+
+        if not observations: #Stop loop if fetch_observations() returns empty page
+            break
+
+        all_observations.extend(observations)
+
+        print(f"Fetched page {page}: {len(observations)} observations")
+
+    return all_observations
+
+def fetch_observations(species_name, page=1):
+    url = 'https://api.inaturalist.org/v1/observations'
 
     params = {
         'taxon_name': species_name,
         'quality_grade': 'research',
         'per_page': 10,
+        'page': page
         }
 
     response = requests.get(url, params=params)
@@ -70,13 +77,13 @@ def save_geojson(features):
     with open("../observations/observations.geojson", "w", encoding="utf-8") as file:
         json.dump(geojson, file, indent=2)
 
+if __name__ == "__main__":
 
+    observations = fetch_all_observations(species_name)
 
-observations = fetch_observations(species_name)
-
-features = create_features(observations)
-        
-geojson = save_geojson(features)
+    features = create_features(observations)
+            
+    geojson = save_geojson(features)
 
 
 
