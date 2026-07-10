4341ed9e73f1d03a16ebfc465d918530d6217ad0
Lucas Smith <smitlucas2563@gmail.com>
2026-07-10 12:48:30 -0400

    Refactor into funtions

--- python/update_observation.py
+++ python/update_observation.py
@@ -9,50 +9,39 @@
 import requests
 import json
 
-url = 'https://api.inaturalist.org/v1/observations'
-
 species_name = 'Callicarpa americana'
 
-params = {
-    'taxon_name': species_name,
-    'quality_grade': 'research',
-    'per_page': 10,
-    }
+def fetch_observations(species_name):
+    url = 'https://api.inaturalist.org/v1/observations'
 
-response = requests.get(url, params=params)
-data = response.json()
 
-observations = data['results']
-
-print(f'Found {len(observations)} observations for {species_name}')
-
-features = []
-
-#Extract species, date, lattitude, longitude
-for observation in observations:
-
-    taxon = observation["taxon"]
-    observation_id = observation["id"]
-    taxon_id = taxon["id"]
-    common_name = taxon["preferred_common_name"]
-    scientific_name = taxon['name']
-    observed_on = observation.get('observed_on')
-    uri = observation['uri']
-    rank = taxon['rank']
-    iconic_taxon = taxon['iconic_taxon_name']
-
-    #print(json.dumps(observation["geojson"], indent=2))
-
-    properties = {
-            "observation_id": observation_id,
-            "taxon_id": taxon_id,
-            "scientific_name": scientific_name,
-            "common_name": common_name,
-            "observed_on": observed_on,
-            "uri": uri,
-            "taxon_rank": rank,
+    params = {
+        'taxon_name': species_name,
+        'quality_grade': 'research',
+        'per_page': 10,
         }
 
+    response = requests.get(url, params=params)
+    data = response.json()
+    
+    return data["results"]
+
+
+
+#Extract species, date, lattitude, longitude
+def create_feature(observation):
+    taxon = observation["taxon"]
+
+    properties = {
+            "observation_id": observation["id"],
+            "taxon_id": taxon["id"],
+            "scientific_name": taxon["name"],
+            "common_name": taxon.get("prefered_common_name"),
+            "observed_on": observation.get("observed_on"),
+            "uri": observation["uri"],
+            "taxon_rank": taxon["rank"],
+            "iconic_taxon": taxon["iconic_taxon_name"]
+            }
 
     feature = {
             "type": "Feature",
@@ -60,15 +49,26 @@ for observation in observations:
             "properties" : properties
             }
     
+    return feature
+
+
+observations = fetch_observations(species_name)
+
+features = []
+
+for observation in observations:
+    feature = create_feature(observation)
     features.append(feature)
-    
 
 geojson = {
         "type": "FeatureCollection",
-        "features": features
+        "features": features 
         }
 
 print(json.dumps(geojson, indent=2))
 
 with open("../observations/observations.geojson", "w", encoding="utf-8") as file:
     json.dump(geojson, file, indent=2)
+
+
+
