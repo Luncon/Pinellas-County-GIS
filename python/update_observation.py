2178cda4065cc13064da128528c8141973847f48
Lucas Smith <smitlucas2563@gmail.com>
2026-07-10 11:56:28 -0400

    Mapped properties and geometry to feature GeoJSON

--- python/update_observation.py
+++ python/update_observation.py
@@ -7,6 +7,7 @@
 # 4. Print total observations found
 
 import requests
+import json
 
 url = 'https://api.inaturalist.org/v1/observations'
 
@@ -26,11 +27,46 @@ observations = data['results']
 print(f'Found {len(observations)} observations for {species_name}')
 
 
+#Extract species, date, lattitude, longitude
+observation = observations[0]
+
+taxon = observation["taxon"]
+
+observation_id = observation["id"]
+taxon_id = taxon["id"]
+common_name = taxon["preferred_common_name"]
+scientific_name = taxon['name']
+observed_on = observation.get('observed_on')
+uri = observation['uri']
+rank = taxon['rank']
+iconic_taxon = taxon['iconic_taxon_name']
+
+#print(json.dumps(observation["geojson"], indent=2))
+
+
+properties = {
+        "observation_id": observation_id,
+        "taxon_id": taxon_id,
+        "scientific_name": scientific_name,
+        "common_name": common_name,
+        "observed_on": observed_on,
+        "uri": uri,
+        "taxon_rank": rank,
+    }
+
+
+feature = {
+        "type": "Feature",
+        "geometry": observation["geojson"],
+        "properties" : properties
+        }
+
+print(json.dumps(feature, indent=2))
+
 for index, observation in enumerate(observations, start=1):
     species = observation.get('species_guess')
     observed_on = observation.get('observed_on')
     location = observation.get('location')
 
     print(f"{index}. {species} | {observed_on} | {location}")
-
 
